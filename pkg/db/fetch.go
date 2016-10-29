8819d9fa016eef6054c90d657aad8bd01a29bab8
Michael Orr <michael@orr.co>
2016-10-29 10:23:39 -0400

    add endpoints for specific metrics

--- pkg/db/fetch.go
+++ pkg/db/fetch.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"time"
 
@@ -50,4 +51,56 @@ func LatestFromBucket(tx *bolt.Tx, bucketName string) ([]byte, []byte) {
 	}
 	c := b.Cursor()
 	return c.Last()
+}
+
+func BucketPayload(conn *bolt.DB, bucketName string) ([]byte, error) {
+	// This func is hardcoded to 10 objects in the response, it would be nice
+	// to parameterize this value but that would require using something other
+	// than DefaultServeMux
+	metricSlice := make([]metrics.JsonMetric, 0)
+
+	if err := conn.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		if b == nil {
+			return fmt.Errorf("Unable to fetch bucket: %s", bucketName)
+		}
+
+		c := b.Cursor()
+		k, v := c.Last()
+		metricSlice = insertIntoPayload(metricSlice, bucketName, k, v)
+
+		for i := 0; i < 9; i++ {
+			k, v := c.Prev()
+			metricSlice = insertIntoPayload(metricSlice, bucketName, k, v)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	response := metrics.JsonPayload{time.Now().UTC().Format("2006-01-02T15:04:05.999"), metricSlice}
+	return json.Marshal(response)
+}
+
+func insertIntoPayload(metricSlice []metrics.JsonMetric, metricName string, key_b, val_b []byte) []metrics.JsonMetric {
+	// if Last() or Prev() don't return valid data, we can't insert it
+	if key_b == nil {
+		log.Println("Error fetching value, skipping")
+		return metricSlice
+	}
+
+	val_f, err := Btof(val_b)
+	if err != nil {
+		log.Printf("There was an error converting %s to float64", val_b)
+		return metricSlice
+	}
+
+	data := metrics.JsonMetric{
+		DataPoint: metrics.DataPoint{
+			Name:  metricName,
+			Value: val_f,
+		},
+		Timestamp: string(key_b),
+	}
+	return append(metricSlice, data)
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/boltdb/bolt"
 
 	"github.com/michaelorr/goodall/pkg/db"
+	"github.com/michaelorr/goodall/pkg/metrics"
 )
 
 func Run(conn *bolt.DB, port int) {
@@ -20,6 +21,19 @@ func Run(conn *bolt.DB, port int) {
 		}
 		fmt.Fprintf(w, "%s", response)
 	})
+
+	for bucket, _ := range metrics.BucketMap {
+		bucketName := bucket // needed to bring this arg into the closure
+		http.HandleFunc(fmt.Sprintf("/%s", bucket), func(w http.ResponseWriter, r *http.Request) {
+			response, err := db.BucketPayload(conn, bucketName)
+			if err != nil {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			fmt.Fprintf(w, "%s", response)
+		})
+	}
 
 	log.Printf("listening on http://127.0.0.1:%d/\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
